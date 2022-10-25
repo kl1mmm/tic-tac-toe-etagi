8e8b0c4587ab097947f008d79567f87287726526
kl1mmm <klimovilya07@mail.ru>
2022-10-25 18:50:32 +0300

    Inputs are created

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script>
+<template>
+  <input-text placeholder="Проверка"></input-text>
+</template>
 
+<script>
+import InputText from "@/components/UI/InputText";
+export default {
+  components: {InputText}
+}
 </script>
 
 <style>
-
+* {
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 </style>
