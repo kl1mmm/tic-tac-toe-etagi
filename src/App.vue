cb2f107d113cc00347fce7e03fd2e5252afa1076
kl1mmm <klimovilya07@mail.ru>
2022-11-02 13:56:51 +0300

    Statuses are emitted.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div style="font-size: 64px; font-weight: bolder;">Project in work.</div>
+  <MyStatus class="Free M" style="width: 100px; height: 32px;">Готов</MyStatus>
 </template>
 
 <script>
-
+import MyStatus from "@/components/UI/MyStatus";
+export default {
+  components: {MyStatus}
+}
 </script>
 
 <style>
---
+++ src/components/UI/MyStatus.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyStatus"
+}
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+  vertical-align: middle;
+  font-weight: 400;
+}
+.Free {
+  background: #A8D37F;
+}
+
+.Blocked {
+  background: #EC7777;
+}
+
+.InGame {
+  background: #87CAE8;
+}
+
+.OutOfGame {
+  background: #EDEDED;
+  color: #373745;
+}
+
+.M {
+  padding: 4px 12px;
+  gap: 4px;
+  border-radius: 6px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 32px;
+  color: #FFFFFF;
+}
+
+.S {
+  padding: 4px;
+  gap: 4px;
+  border-radius: 4px;
+  font-size: 12px;
+  line-height: 24px;
+  letter-spacing: 0.25px;
+  color: #FFFFFF;
+}
+</style>
